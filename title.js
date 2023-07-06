6262ff42e80f8e7e347849dba039d74a4d8ee7a3
ploxeret <134236113+ploxerets@users.noreply.github.com>
2023-07-06 13:18:15 -0700

    title.js

--- title.js
+++ title.js
@@ -1,8 +1,8 @@
 function console_title(x) {
     if (process.platform == 'win32') {
-        process.title = x + " / FastBomber v1.0 - github.com/fastuptime";
+        process.title = x + " / FastBomber v1.0 - github.com/ploxerets";
     } else {
-        process.stdout.write('\x1b]2;' + x + " / FastBomber v1.0 - github.com/fastuptime" + '\x1b\x5c');
+        process.stdout.write('\x1b]2;' + x + " / FastBomber v1.0 - github.com/ploxerets" + '\x1b\x5c');
     }
 }
 
